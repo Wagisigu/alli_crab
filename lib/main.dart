383cdf627b8a3055402660a7e6ad2d4c5572bc60
Cameron Wagner <55332052+Wagisigu@users.noreply.github.com>
2022-06-30 15:26:34 -0500

    can now click on reviews and lessons and get the first item to learn

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.grey,
       ),
       home: getWelcome(prefs),
-      //home: const MyHomePage(title: 'AlliCrab'),
     );
   }
 
@@ -31,8 +30,8 @@ class MyApp extends StatelessWidget {
         if (snapshot.hasData) {
           SharedPreferences? prefs = snapshot.data;
           String? apiKey = prefs?.getString('apiKey');
-          if (apiKey != null) {
-            return MyHomePage(apiKey: apiKey);
+          if (apiKey != null && prefs != null) {
+            return MyHomePage(apiKey: apiKey, prefs: prefs);
           } else if (prefs != null) {
             return LoginPage(prefs: prefs);
           }
@@ -96,7 +95,7 @@ class _LoginPageState extends State<LoginPage> {
                 var json = jsonDecode(x.body);
                 if (json['object'].toString() == 'collection') {
                   widget.prefs.setString('apiKey', apiTextFieldController.text);
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage(apiKey: apiTextFieldController.text)));
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage(apiKey: apiTextFieldController.text, prefs: widget.prefs)));
                 } else {
                   setState(() => {response = 'Invalid Key'});
                 }
@@ -112,9 +111,10 @@ class _LoginPageState extends State<LoginPage> {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.apiKey}) : super(key: key);
+  const MyHomePage({Key? key, required this.apiKey, required this.prefs}) : super(key: key);
 
   final String apiKey;
+  final SharedPreferences prefs;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -139,19 +139,15 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: Material(
           child: GridView.count(
-            crossAxisCount: 2,
+            crossAxisCount: 1,
             children: [
               Material(
-                child: getSquare(lessonCount),
+                child: getSquare("lessons\n", lessonCount),
                 color: Colors.blue[500],
               ),
               Material(
-                child: getSquare(reviewCount),
+                child: getSquare("reviews\n", reviewCount),
                 color: Colors.orange[500],
-              ),
-              Material(
-                child: getInkSquare('graph'),
-                color: Colors.green[500],
               ),
             ],
           ),
@@ -162,22 +158,25 @@ class _MyHomePageState extends State<MyHomePage> {
   InkWell getInkSquare(String text) {
     return InkWell(
       onTap: () => {
-        setState(() => {})
+        setState(() => {}),
+        Navigator.push(context, MaterialPageRoute(builder: (context) => LearnPage(apiKey: widget.apiKey)))
       },
       child: FittedBox(
         fit: BoxFit.contain,
-        child: Text(text),
+        child: Text(text,
+        textAlign: TextAlign.center
+        ),
       ),
       splashColor: Colors.white,
     );
   }
 
-  FutureBuilder<String> getSquare(Future<String> toCome) {
+  FutureBuilder<String> getSquare(String name, Future<String> toCome) {
     return FutureBuilder<String>(
     future: toCome,
     builder: (context, snapshot) {
       if (snapshot.hasData) {
-        return getInkSquare(snapshot.data!.toString());
+        return getInkSquare(name+snapshot.data!.toString());
       } else if (snapshot.hasError) {
         return getInkSquare('${snapshot.error}');
       }
@@ -192,6 +191,89 @@ class _MyHomePageState extends State<MyHomePage> {
         headers: {"Authorization" : "Bearer "+apiKey});
     var json = jsonDecode(x.body);
     for (String s in path) {
+      json = json[s];
+    }
+    return json.toString();
+  }
+}
+
+class LearnPage extends StatefulWidget {
+  const LearnPage({Key? key, required this.apiKey}) : super(key: key);
+
+  final String apiKey;
+
+  @override
+  State<StatefulWidget> createState() => _LearnPageState();
+}
+
+class _LearnPageState extends State<LearnPage> {
+
+  final answerTextFieldController = TextEditingController();
+  late String seen;
+
+  @override
+  void dispose() {
+    answerTextFieldController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Allicrab'),
+      ),
+      body: Material(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FutureBuilder<String>(
+              future: getImage(getWord()),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  String? s = snapshot.data;
+                  if (s != null) {
+                    seen = s;
+                    return Text(s);
+                  }
+                  return const Text('done');
+                }
+                return const CircularProgressIndicator();
+              },
+            ),
+            TextField(
+              controller: answerTextFieldController,
+            ),
+            TextButton(
+              onPressed: () async {
+                if (answerTextFieldController.text == seen) {
+
+                }
+              },
+              child: const Text('Submit')
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  
+  Future<String> getWord() async {
+    return submitApiGet("https://api.wanikani.com/v2/assignments", ['data',0,'data','subject_id'], widget.apiKey);
+  }
+
+  Future<String> getImage(Future<String> url) async {
+    String u = '';
+    await url.then((value) => u = value);
+    return submitApiGet('https://api.wanikani.com/v2/subjects/'+u, ['data','characters'], widget.apiKey);
+
+  }
+
+  Future<String> submitApiGet(String url, var path, String apiKey) async {
+    final x = await http.get(Uri.parse(url),
+        headers: {"Authorization" : "Bearer "+apiKey});
+    var json = jsonDecode(x.body);
+    for (var s in path) {
       json = json[s];
     }
     return json.toString();
